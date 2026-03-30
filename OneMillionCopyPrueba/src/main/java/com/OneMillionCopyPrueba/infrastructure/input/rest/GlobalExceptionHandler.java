d23b9703f6bb5e5399cad7fc404d5f8cd8e0868b
Fernando Carta <cartafernando@outlook.com>
2026-03-30 13:03:51 -0500

    Manejo de error para fuente diferente a las opciones disponibles

--- OneMillionCopyPrueba/src/main/java/com/OneMillionCopyPrueba/infrastructure/input/rest/GlobalExceptionHandler.java
+++ OneMillionCopyPrueba/src/main/java/com/OneMillionCopyPrueba/infrastructure/input/rest/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.OneMillionCopyPrueba.infrastructure.input.rest;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,4 +17,26 @@ public class GlobalExceptionHandler {
     public String handleDuplicateEmail(DuplicateEmailException ex) {
         return ex.getMessage();
     }
+
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleInvalidEnum(HttpMessageNotReadableException ex) {
+
+        if (ex.getMessage().contains("Fuente")) {
+            return Map.of(
+                "error", "FUENTE_INVALIDA",
+                "message", "La fuente debe ser: INSTAGRAM, FACEBOOK, LANDING_PAGE, REFERIDO, OTRO"
+            );
+        }
+
+        return Map.of(
+            "error", "JSON_INVALIDO",
+            "message", "Error en el formato del request"
+        );
+    }
+
+
+
 }
