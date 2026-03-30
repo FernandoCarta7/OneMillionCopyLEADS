a47a9e27d38f146b1de41d2fba8f3c7299c0c97f
Fernando Carta <cartafernando@outlook.com>
2026-03-30 13:12:27 -0500

    Manejo de error para nombre y longitud

--- OneMillionCopyPrueba/src/main/java/com/OneMillionCopyPrueba/infrastructure/input/rest/GlobalExceptionHandler.java
+++ OneMillionCopyPrueba/src/main/java/com/OneMillionCopyPrueba/infrastructure/input/rest/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.OneMillionCopyPrueba.infrastructure.input.rest;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,25 +20,42 @@ public class GlobalExceptionHandler {
         return ex.getMessage();
     }
 
-
-
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleInvalidEnum(HttpMessageNotReadableException ex) {
 
         if (ex.getMessage().contains("Fuente")) {
             return Map.of(
-                "error", "FUENTE_INVALIDA",
-                "message", "La fuente debe ser: INSTAGRAM, FACEBOOK, LANDING_PAGE, REFERIDO, OTRO"
-            );
+                    "error", "FUENTE_INVALIDA",
+                    "message", "La fuente debe ser: INSTAGRAM, FACEBOOK, LANDING_PAGE, REFERIDO, OTRO");
         }
 
         return Map.of(
-            "error", "JSON_INVALIDO",
-            "message", "Error en el formato del request"
-        );
+                "error", "JSON_INVALIDO",
+                "message", "Error en el formato del request");
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIllegalArgument(IllegalArgumentException ex) {
+        return Map.of(
+                "error", "VALIDACION_NEGOCIO",
+                "message", ex.getMessage());
+    }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleValidationErrors(MethodArgumentNotValidException ex) {
+
+        List<String> errores = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        return Map.of(
+                "error", "VALIDATION_ERROR",
+                "messages", errores);
+    }
 
 }
