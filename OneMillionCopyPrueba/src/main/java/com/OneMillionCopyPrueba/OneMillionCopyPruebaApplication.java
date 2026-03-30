dd7081c4ccd7ebe24f2448c57410db5e14e839e2
Fernando Carta <cartafernando@outlook.com>
2026-03-30 10:26:06 -0500

    Creacion proyecto en java, spring boot con dependencias y configuracion de base de datos en mysql

---
+++ OneMillionCopyPrueba/src/main/java/com/OneMillionCopyPrueba/OneMillionCopyPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.OneMillionCopyPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMillionCopyPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneMillionCopyPruebaApplication.class, args);
+	}
+
+}
---
+++ OneMillionCopyPrueba/src/test/java/com/OneMillionCopyPrueba/OneMillionCopyPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OneMillionCopyPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneMillionCopyPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
